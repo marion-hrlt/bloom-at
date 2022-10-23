e52473cc554d65b58972addbd754d9f3cf148d9c
Marion <marion@krealab.fr>
2022-10-23 16:09:24 +0200

    💄 Fix templating

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,17 @@
 module.exports = {
-  purge: [],
-  darkMode: 'class', // or 'media' or 'class'
-  theme: {
-    extend: {
-      colors: {
-        'darkest': '#373737',
-        'darkbg': '#4D4D4D',
-        'darkmobile': '#292929',
-      },
-    },
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [
-    require('@tailwindcss/forms'),
-  ],
-}
+	purge: [],
+	darkMode: "class", // or 'media' or 'class'
+	theme: {
+		extend: {
+			colors: {
+				darkest: "#373737",
+				darkbg: "#4D4D4D",
+				darkmobile: "#292929",
+			},
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [require("@tailwindcss/forms")],
+};
